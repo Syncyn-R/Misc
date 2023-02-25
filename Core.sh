7d38a52eca1925ef282b9902b81c1381ea14ef7e
Hash_Define <69207050+Hash-Def1ne@users.noreply.github.com>
2023-02-25 21:10:58 +0800

    Update Core.sh

--- Core.sh
+++ Core.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-roomid=$1
 logdir=logs
 outputformat=mkv
 corelogfilename=`date +'Core-%Y-%m-%d_%H-%M-%S.log'`
@@ -9,12 +8,12 @@ tlog(){
     echo [`date +'%Y-%m-%d %H:%M:%S'`] $* | tee -a $logdir/$corelogfilename
 }
 
-while true
-do
+record(){
+	roomid=$1
 	filename=`date +'%Y-%m-%d_%H-%M-%S'`
-	[ ! -d $logdir ] && mkdir $logdir
-	if [[ `curl -s https://api.live.bilibili.com/room/v1/Room/room_init?id=$roomid | grep '"live_status":1'` != '' ]]
+	if [[ `curl -s 'https://api.live.bilibili.com/room/v1/Room/room_init?id='$roomid | jq '.data.live_status' -r` == '1' ]]
 	then
+		tlog Detected $roomid status 1.
 		sourceaddress=`curl -s 'https://api.live.bilibili.com/room/v1/Room/playUrl?cid='$roomid'&qn=10000&platform=web' | jq '.data.durl[0].url' -r`
 		tlog Started $filename
 		ffmpeg -user_agent 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36' -headers 'referer: https://live.bilibili.com/'$roomid -re -i $sourceaddress -c copy $filename.$outputformat > $logdir/Record-$filename.log 2>&1
@@ -24,4 +23,18 @@ do
 		grep -q 'HTTP error 404 Not Found' $logdir/Record-$filename.log
 		[ $? -eq 0 ] && rm logs/Record-$filename.log && tlog Deleted 404 $logdir/Record-$filename.log
 	fi
-done
+}
+
+loop(){
+	[ ! -d $logdir ] && mkdir $logdir
+	tlog Script Started.
+	while true
+	do
+		$0 record $1
+	done
+}
+
+case "$1" in
+record) record $2;;
+$1) loop $1;;
+esac
